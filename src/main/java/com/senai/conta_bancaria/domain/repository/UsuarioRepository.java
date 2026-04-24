210c8c058307168b4633a73edcd5831d7a5c0fa3
Rai <raifelipeafonso@gmail.com>
2026-04-24 14:24:17 -0300

    correção bug

--- src/main/java/com/senai/conta_bancaria/domain/repository/UsuarioRepository.java
+++ src/main/java/com/senai/conta_bancaria/domain/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.senai.conta_bancaria.domain.repository;
 import com.senai.conta_bancaria.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
+    Optional<Object> findByEmail(String adminEmail);
 }
